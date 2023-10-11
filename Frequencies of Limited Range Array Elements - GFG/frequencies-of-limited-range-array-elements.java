e3114f763c42b68a2105a5f1ba61a2341d567b99
Utkarsh Kumar <utkarsh70888@gmail.com>
2023-10-11 13:11:07 +0530

    Added solution - LeetHub

---
+++ Frequencies of Limited Range Array Elements - GFG/frequencies-of-limited-range-array-elements.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+        int t = Integer.parseInt(br.readLine().trim()); 
+        while(t-->0){
+            
+            //size of array
+            int N = Integer.parseInt(br.readLine().trim()); 
+            int arr[] = new int[N];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            //adding elements to the array
+            for(int i = 0 ; i < N; i++){
+                arr[i]=Integer.parseInt(inputLine[i]); 
+            }
+            int P= Integer.parseInt(br.readLine().trim());
+            //calling frequncycount() function
+            Solution ob = new Solution();
+            ob.frequencyCount(arr, N, P); 
+            
+            //printing array elements
+            for(int i = 0; i < N ; i++)
+                System.out.print(arr[i] + " " );
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    public static void frequencyCount(int arr[], int N, int P){
+        TreeMap<Integer,Integer>map = new TreeMap();
+        for(int i:arr) map.put(i,map.getOrDefault(i,0)+1);
+        for(int i=0;i<N;i++) arr[i] = map.containsKey(i+1)?map.get(i+1):0;
+    }
+}
