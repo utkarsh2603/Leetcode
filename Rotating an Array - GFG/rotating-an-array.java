35acdba47e34e438289d903b5bed5f5e724f7602
Utkarsh Kumar <utkarsh70888@gmail.com>
2023-10-21 09:19:41 +0530

    Added solution - LeetHub

---
+++ Rotating an Array - GFG/rotating-an-array.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int d = Integer.parseInt(br.readLine().trim());
+
+            new Solution().leftRotate(arr, n, d);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void leftRotate(int[] arr, int n, int d) {
+        // code here
+         int[] result = new int[n*2];
+        System.arraycopy(arr, 0, result, 0, n);
+        System.arraycopy(arr, 0, result, n, n);
+        System.arraycopy(result,d,arr,0,n);
+
+
+    }
+}
