9a6d1e2595ff81e4e811f5d16855b32b8aad453a
Utkarsh Kumar <utkarsh70888@gmail.com>
2023-10-28 01:24:33 +0530

    Added solution - LeetHub

---
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.java
@@ -0,0 +1,149 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class Is_LinkedList_Palindrom
+{
+	 Node head;  
+	 Node tail;
+	
+	/* Function to print linked list */
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+ 
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) 
+	{
+
+		if (head == null) 
+		{
+			head = node;
+			tail = node;
+		} else 
+		{
+		    tail.next = node;
+		    tail = node;
+		}
+    }
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		 
+		while(t>0)
+        {
+			int n = sc.nextInt();
+			//int k = sc.nextInt();
+			Is_LinkedList_Palindrom llist = new Is_LinkedList_Palindrom();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            Node tail = head;
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+			    tail.next = new Node(a);
+			    tail = tail.next;
+			}
+			
+			Solution g = new Solution();
+			if(g.isPalindrome(head) == true)
+			    System.out.println(1);
+		    else
+			    System.out.println(0);
+			t--;
+		}
+		
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+/* Structure of class Node is
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+
+class Solution {
+
+    // Function to check whether the list is palindrome.
+
+    boolean isPalindrome(Node head) {
+
+        Node slow = head, fast = head;
+
+        int sh = 0, fh = 0;
+
+        int i = 1;
+
+        while (fast.next != null && fast.next.next != null) {
+
+            fh= slow.data * i + fh;
+
+            slow = slow.next;
+
+            fast = fast.next.next;
+
+            i *= 10;
+
+        }
+
+        if (fast.next != null) {
+
+            fh += slow.data * i;
+
+        }
+
+        slow = slow.next;
+
+        i = 1;
+
+        while (slow != null) {
+
+            sh = sh * i + slow.data;
+
+            slow = slow.next;
+
+            i *= 10;
+
+        }
+
+        return fh == sh;
+
+    }
+
+}
+
